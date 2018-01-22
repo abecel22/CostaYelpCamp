aac10e2779e5706d30ec4a175de2e5a7c6f313b8
abecel22 <abe2211@gmail.com>
2018-01-21 21:49:43 -0600

    Completed first express test app.

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,2 +1,36 @@
 var express = require('express');
 var app = express();
+
+// '/' => 'Hi there!'
+app.get('/', function(req, res){
+    res.send('Hi there!');
+});
+// '/bye' => 'Goodbye!!'
+app.get('/bye', function(req, res) {
+    res.send('Goodbye!!');
+});
+// '/dog' => 'Ruff!'
+app.get('/dog', function(req, res) {
+    console.log('Someone Made a request to /dog!')
+    res.send('Ruff!!');
+});
+
+app.get('/r/:subredditName', function(req, res) {
+    let subreddit = req.params.subredditName;
+    res.send('Welcome to the ' + subreddit + ' subreddit!!');
+});
+
+app.get('/r/:subredditName/comments/:id/:title', function(req, res) {
+    console.log(req.params);
+    res.send('Welcome to the comments!!');
+});
+
+app.get('*', function(req, res) {
+    res.send('You are a Star!!');
+});
+
+// Tell Express to listen for requests (start server)
+
+app.listen(3000, function() {
+    console.log('Server has started!');
+});
