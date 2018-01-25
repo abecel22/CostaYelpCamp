cff9bc530b15601c63461f7643d345b561266655
abecel22 <abe2211@gmail.com>
2018-01-24 21:17:49 -0600

    Learned Ifs and loops inside an ejs file.

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -5,12 +5,20 @@ app.get('/', function(req, res) {
     res.render('home.ejs'); 
 })
 
-//fallinlovewith/rusty 
+//fallinlovewith/bruno (my dog) 
 app.get('/fallinlovewith/:thing', function(req, res) {
     let thing = req.params.thing;
     res.render('love.ejs', {thingVar: thing});
 });
 
+app.get('/posts', function(req, res) {
+    let posts = [
+        {title: 'Bruno the Puppy' , author: 'Cely'},
+        {title: 'My Adorable Pet Turtle', author: 'Gabs'},
+        {title: 'Can You Believe This Pig?', author: 'Sofs'}
+    ]
+    res.render('posts.ejs', {posts: posts});
+});
 
 app.listen(3000, function(req, res) {
     console.log('Server is listening!');
