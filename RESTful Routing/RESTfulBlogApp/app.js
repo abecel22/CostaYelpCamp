dece4133f2ceada245486ca05c503e583e4db651
abecel22 <abe2211@gmail.com>
2018-02-01 22:03:35 -0600

    Added semantic ui to blog app. Added footer & header.

--- RESTful Routing/RESTfulBlogApp/app.js
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -1,20 +1,20 @@
 const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      mongoose = require('mongoose');
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 //app config
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Mongoose/ model config
 let blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 });
 
 let Blog = mongoose.model('Blog', blogSchema);
@@ -26,14 +26,13 @@ app.get('/', function(req, res) {
 
 app.get('/blogs', function(req, res) {
     Blog.find({}, function(err, blogs) {
-        if(err) {
+        if (err) {
             console.log(err);
         } else {
-            res.render('index', {blogs: blogs});
+            res.render('index', { blogs: blogs });
         }
     });
 });
-
 
 app.listen(3000, function() {
     console.log('Server is running!');
