f02350ebdf8b0c392357b773f8acdbd8a448681c
abecel22 <abe2211@gmail.com>
2018-02-11 18:59:02 -0600

    Added update routes to campground

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -66,6 +66,33 @@ router.get('/campgrounds/:id', function(req, res) {
         });
 });
 
+//edit campground routes
+router.get('/campgrounds/:id/edit', function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.render('campgrounds/edit', { campground: foundCampground });
+        }
+    });
+});
+
+//update campground route
+router.put('/campgrounds/:id', function(req, res) {
+    //find and update correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(
+        err,
+        updatedCampground
+    ) {
+        if (err) {
+            res.redirect('/campgrounds');
+        } else {
+            //redirect show page
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
 //middleware to check if user is logged in
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
